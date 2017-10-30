ed240555ab4df250d6d13a3bd899aa078704174e
Megan <megan.lkm@gmail.com>
2017-10-30 13:35:24 -0500

    initial

---
+++ aws_decorators/__init__.py
@@ -0,0 +1,44 @@
+import os
+from functools import wraps
+from inspect import getargspec
+
+import boto3
+
+AWS_REGION = os.getenv('AWS_REGION', 'us-east-1')
+
+
+def get_client(name, client_type='client', region=None, *args, **kwargs):
+    if region is None:
+        region = AWS_REGION
+    fn = getattr(boto3, client_type)
+    return fn(name, region_name=region, **kwargs)
+
+
+def boto_client(service_name, client_type='client', region=AWS_REGION,
+                client_param_name='client', region_param_name='region'):
+    def _get_client(func):
+        @wraps(func)
+        def make_client(*args, **kwargs):
+            fn_spec = getargspec(func)
+            # create a dict with the fn's kwargs and default values
+            params = dict(
+                zip(fn_spec.args[-len(fn_spec.defaults):], fn_spec.defaults)
+            )
+
+            # update params with arg values and keys
+            params.update(dict((zip(fn_spec.args[:len(args)], args))))
+            params.update(kwargs)
+
+            if params.get(region_param_name) is None:
+                params[region_param_name] = region
+
+            if params.get(client_param_name) is None:
+                params[client_param_name] = get_client(
+                    service_name,
+                    client_type,
+                    params[region_param_name]
+                )
+
+            return func(**params)
+        return make_client
+    return _get_client
---
+++ setup.py
@@ -0,0 +1,21 @@
+import os
+from pip.req import parse_requirements
+from pip.download import PipSession
+from setuptools import setup, find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+INSTALL_REQS = parse_requirements('requirements.txt', session=PipSession())
+
+setup(
+    name='aws-decorators',
+    version='0.0.1',
+    packages=find_packages(exclude=['tests']),
+    include_package_data=True,
+    description='decorators for aws things',
+    long_description='decorators for aws things',
+    url='https://github.com/meganlkm/aws-decorators',
+    author='meganlkm',
+    author_email='devstuff.io@gmail.com',
+    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+)
