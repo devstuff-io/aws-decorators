110410dce717b52a4035f46660f71b7557b51341
Megan <megan.lkm@gmail.com>
2017-10-31 14:15:54 -0500

    things

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 import os
-from pip.req import parse_requirements
-from pip.download import PipSession
+# from pip.req import parse_requirements
+# from pip.download import PipSession
 from setuptools import setup, find_packages
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
-INSTALL_REQS = parse_requirements('requirements.txt', session=PipSession())
+# INSTALL_REQS = parse_requirements('requirements.txt', session=PipSession())
+# install_requires=[str(ir.req) for ir in INSTALL_REQS]
 
 setup(
     name='aws-decorators',
-    version='0.0.1',
+    version='0.0.2',
     packages=find_packages(exclude=['tests']),
     include_package_data=True,
     description='decorators for aws things',
@@ -17,5 +18,5 @@ setup(
     url='https://github.com/meganlkm/aws-decorators',
     author='meganlkm',
     author_email='devstuff.io@gmail.com',
-    install_requires=[str(ir.req) for ir in INSTALL_REQS]
+    install_requires=['boto3']
 )
