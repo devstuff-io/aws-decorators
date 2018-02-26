23d0b1ebd7a21109aba6291cf320b81109009523
Megan <megan.lkm@gmail.com>
2018-02-26 17:48:07 -0600

    move version to init and other things...

--- setup.py
+++ setup.py
@@ -1,12 +1,39 @@
+import ast
 import os
+import re
 from setuptools import setup, find_packages
+
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
+BASEDIR = os.path.dirname(os.path.abspath(__file__))
+VERSION = None
+REQUIREMENTS = []
+DEPENDENCY_LINKS = []
+
+with open(os.path.join(BASEDIR, 'requirements.txt')) as fp:
+    lines = fp.readlines()
+    for line in lines:
+        line = line.strip()
+        if ("http://" in line or "https://" in line or "ssh://" in line) and "#egg=" in line:
+            parts = line.split("#egg=")
+            REQUIREMENTS.append(parts[-1])
+            DEPENDENCY_LINKS.append(line)
+        elif len(line) and line[0] != "#" and line[0] != "-":
+            REQUIREMENTS.append(line)
+
+
+_version_re = re.compile(r'__version__\s+=\s+(.*)')
+with open('aws_decorators/__init__.py', 'rb') as f:
+    VERSION = str(ast.literal_eval(
+        _version_re.search(f.read().decode('utf-8')).group(1)
+    ))
+
+
 setup(
     name='aws-decorators',
-    version='0.0.7',
+    version=VERSION,
     license='MIT',
     packages=find_packages(),
     include_package_data=True,
@@ -15,8 +42,12 @@ setup(
     url='https://github.com/meganlkm/aws-decorators',
     author='meganlkm',
     author_email='devstuff.io@gmail.com',
-    keywords=['aws', 'decorators'],
-    install_requires=['boto3>=1.4.3,<=1.5.0'],
+    install_requires=REQUIREMENTS,
+    dependency_links=DEPENDENCY_LINKS,
+    keywords=[
+        'aws',
+        'decorators'
+    ],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
